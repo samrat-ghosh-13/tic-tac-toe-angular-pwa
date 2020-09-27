6ae66e75fa6831eb5e92fc0f55300a2fb113b443
Samrat Ghosh <samratat@gmail.com>
2020-09-28 03:41:27 +0530

    Fixed the Test Errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tic-tac-toe-angular'`, () => {
+  it(`should have as title 'tic tac toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tic-tac-toe-angular');
+    expect(app.title).toEqual('tic tac toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tic-tac-toe-angular app is running!');
+    expect(compiled.querySelector('.title').textContent).toContain('Tic Tac Toe!');
   });
 });
